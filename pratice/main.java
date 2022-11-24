1da983c6b09ab0fbe448d7e33ac1ea3072b94e1d
StebenC0912 <stebenc09122002@gmail.com>
2022-11-24 23:19:58 +0700

    pratice more

--- pratice/main.java
+++ pratice/main.java
@@ -1,84 +1,32 @@
 package pratice;
 
-import java.util.Optional;
-
 public class main {
-
-
     public static void main(String[] args) {
-
-        // create an instance of Account here
-        Account account = new Account("123456", 1000, new User("demo-user","Alexander", "Schmidt"));
-        // pass it into process method
-        process(account);
+        System.out.println("hello");
+        ;
     }
+}
+class Animal {
 
-    static class Account {
-
-        private String code;
-        private long balance;
-        private User owner;
-
-        public Account(String code, long balance, User owner) {
-            this.code = code;
-            this.balance = balance;
-            this.owner = owner;
-        }
-
-        public String getCode() {
-            return code;
-        }
-
-        public long getBalance() {
-            return balance;
-        }
-
-        public User getOwner() {
-            return owner;
-        }
+    public void say() {
+        System.out.println("...An incomprehensible sound...");
     }
+}
 
-    static class User {
-
-        private String login;
-        private String firstName;
-        private String lastName;
-
-        public User(String login, String firstName, String lastName) {
-            this.login = login;
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
+class Cat extends Animal {
+    public void say() {
+        System.out.println("meow-meow");
     }
+}
 
+class Dog extends Animal {
+    public void say() {
+        System.out.println("arf-arf");
+    }
+}
 
-    public static void process(Account account) {
-        try {
-            final Optional<User> owner = Optional.ofNullable(account.getOwner());
-
-            System.out.println(account.getCode());
-            System.out.println(account.getBalance());
-
-            owner.ifPresent(o -> {
-                System.out.println(o.getLogin());
-                System.out.println(o.getFirstName());
-                System.out.println(o.getLastName());
-            });
-
-        } catch (Exception e) {
-            System.out.println("Something wrong...");
-        }
+class Duck extends Animal {
+    public void say() {
+        System.out.println("quack-quack");
     }
 }

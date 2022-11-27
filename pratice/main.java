60d9866a87a68a314ce96b89dbe290e36fd023c3
StebenC0912 <stebenc09122002@gmail.com>
2022-11-27 21:57:53 +0700

    update more

--- pratice/main.java
+++ pratice/main.java
@@ -2,134 +2,78 @@ package pratice;
 
 public class main {
     public static void main(String[] args) {
-        MutableShape circle = new Circle(2.0f, 3.5f, 10.1f);
-
-        circle.move(10.1f, 20.2f);
-        circle.scale(2.2f);
-
-        ((Circle) circle).getRadius();
+        
     }
 }
 
-interface Movable {
+/**
+    Box for cakes
+*/
+class CakeBox {
 
-    void move(float dx, float dy);
-}
+    private Cake cake;
 
-interface Scalable {
-
-    void scale(float factor);
-}
-
-interface MutableShape extends Movable, Scalable {
-
-}
-
-final class Circle implements MutableShape {
-
-    /**
-     * Defines the horizontal position of the center of the circle
-     */
-    private float centerX;
-
-    /**
-     * Defines the vertical position of the center of the circle
-     */
-    private float centerY;
-
-    /**
-     * Defines the radius of the circle
-     */
-    private float radius;
-
-    public Circle(float centerX, float centerY, float radius) {
-        this.centerX = centerX;
-        this.centerY = centerY;
-        this.radius = radius;
+    public void put(Cake cake) {
+    	this.cake = cake;
     }
 
-    public float getCenterX() {
-        return centerX;
-    }
-
-    public float getCenterY() {
-        return centerY;
-    }
-
-    public float getRadius() {
-        return radius;
-    }
-
-    @Override
-    public void move(float dx, float dy) {
-        // TODO Auto-generated method stub
-        this.centerX += dx;
-        this.centerY += dy;
-    }
-
-    @Override
-    public void scale(float factor) {
-        // TODO Auto-generated method stub
-        this.radius *= factor;
+    public Cake get() {
+        return this.cake;
     }
 }
 
-final class Rectangle implements MutableShape{
+/**
+    Box for pies
+*/
+class PieBox {
 
-    /**
-     * Defines the X coordinate of the upper-left corner of the rectangle.
-     */
-    private float x;
+    private Pie pie;
 
-    /**
-     * Defines the Y coordinate of the upper-left corner of the rectangle.
-     */
-    private float y;
-
-    /**
-     * Defines the width of the rectangle.
-     */
-    private float width;
-
-    /**
-     * Defines the height of the rectangle.
-     */
-    private float height;
-
-    public Rectangle(float x, float y, float w, float h) {
-        this.x = x;
-        this.y = y;
-        this.width = w;
-        this.height = h;
+    public void put(Pie pie) {
+    	this.pie = pie;
     }
 
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public float getWidth() {
-        return width;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    @Override
-    public void move(float dx, float dy) {
-        // TODO Auto-generated method stub
-        this.x += dx;
-        this.y += dy;
-    }
-
-    @Override
-    public void scale(float factor) {
-        // TODO Auto-generated method stub
-        this.height *= factor;
-        this.width *= factor;
+    public Pie get() {
+        return this.pie;
     }
 }
+
+
+/**
+    Box for tarts
+*/
+class TartBox {
+
+    private Tart tart;
+
+    public void put(Tart tart) {
+    	this.tart = tart;
+    }
+
+    public Tart get() {
+        return this.tart;
+    }
+}
+
+/*
+    Hundred more such boring classes OR ...
+    magic class for everything everybody is waiting for
+*/
+class Box<T> {
+    private T t;
+    public T getT() {
+        return t;
+    }
+    public void setT(T t) {
+        this.t = t;
+    }
+    
+    
+}
+
+// Don't change classes below
+class Cake { }
+
+class Pie { }
+
+class Tart { }

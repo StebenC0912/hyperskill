9ecfb983ac0d3d87cfc0e5291cc807a8dda8c54a
StebenC0912 <stebenc09122002@gmail.com>
2022-11-28 21:06:43 +0700

    more practice

--- pratice/main.java
+++ pratice/main.java
@@ -1,79 +1,27 @@
 package pratice;
 
+import java.util.*;
+
 public class main {
-    public static void main(String[] args) {
-        
-    }
+
+    public static void main(String... args) {
+            // initialize an instance of Holder to make the code below compiled
+            
+            List<Integer> list = new ArrayList<>();
+            
+            
+        }
+
 }
 
-/**
-    Box for cakes
-*/
-class CakeBox {
+class Holder<T> {
+    private T value;
 
-    private Cake cake;
-
-    public void put(Cake cake) {
-    	this.cake = cake;
+    public void set(T value) {
+        this.value = value;
     }
 
-    public Cake get() {
-        return this.cake;
+    public T get() {
+        return value;
     }
 }
-
-/**
-    Box for pies
-*/
-class PieBox {
-
-    private Pie pie;
-
-    public void put(Pie pie) {
-    	this.pie = pie;
-    }
-
-    public Pie get() {
-        return this.pie;
-    }
-}
-
-
-/**
-    Box for tarts
-*/
-class TartBox {
-
-    private Tart tart;
-
-    public void put(Tart tart) {
-    	this.tart = tart;
-    }
-
-    public Tart get() {
-        return this.tart;
-    }
-}
-
-/*
-    Hundred more such boring classes OR ...
-    magic class for everything everybody is waiting for
-*/
-class Box<T> {
-    private T t;
-    public T getT() {
-        return t;
-    }
-    public void setT(T t) {
-        this.t = t;
-    }
-    
-    
-}
-
-// Don't change classes below
-class Cake { }
-
-class Pie { }
-
-class Tart { }

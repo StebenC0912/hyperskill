e6befff10043d9208ae291e24c702de15f351259
StebenC0912 <stebenc09122002@gmail.com>
2022-12-02 23:57:36 +0700

    end practice

--- pratice/_1657_String_is_close.java
+++ pratice/_1657_String_is_close.java
@@ -1,55 +1,79 @@
 package pratice;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 
 public class _1657_String_is_close {
-    public static boolean operation1(String word1, String word2) {
-        HashMap<Character, Integer> count1 = new HashMap<>();
-        HashMap<Character, Integer> count2 = new HashMap<>();
+    public static boolean operation(String word1, String word2) {
+        if (word1.equals(word2)) {
+            return true;
+        }
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        if (word1.length() == 1 && word2.length() == 1) {
+            if (word1.equals(word2)) return true; 
+            else return false;
+        }
+        boolean onlyOneChar1 = false;
+        boolean onlyOneChar2 = false;
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
         char[] word1Arr = word1.toCharArray();
         char[] word2Arr = word2.toCharArray();
         Arrays.sort(word1Arr);
         Arrays.sort(word2Arr);
         int count = 1;
         char temp = word1Arr[0];
-        for (int j = 1; j < word1Arr.length; j++) {
-            if (word1Arr[j] == temp) {
+        int j = 0;
+        for (int i = 1; i < word1Arr.length; i++) {
+            if (word1Arr[i] == temp) {
                 count++;
             } else {
-                count1.put(word1Arr[j], count);
+                if (!word2.contains(Character.toString(temp)))
+                    return false;
+                count1[j] = count;
+                j++;
                 count = 1;
-                temp = word1Arr[j];
+                temp = word1Arr[i];
             }
-            if (j == word1Arr[word1Arr.length - 1]) {
-                count1.put(word1Arr[j], count);
-            }
+
         }
+        if (j < 1) {
+            onlyOneChar1 = true;
+        }
+        count1[j] = count;
+        j = 0;
+        count = 1;
         temp = word2Arr[0];
         for (int i = 1; i < word2Arr.length; i++) {
             if (word2Arr[i] == temp) {
                 count++;
             } else {
-                count2.put(word1Arr[i], count);
+                count2[j] = count;
+                j++;
                 count = 1;
                 temp = word2Arr[i];
             }
-            if (i == word2Arr[word1Arr.length - 1]) {
-                count2.put(word1Arr[i], count);
-            }
+
         }
-        return true;
-    }
-    public static boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) {
+        if (j < 1) {
+            onlyOneChar2 = true;
+        }
+        if (onlyOneChar1 == true && onlyOneChar2 == true) {
             return false;
         }
-
+        count2[j] = count;
+        Arrays.sort(count1);
+        Arrays.sort(count2);
+        if (!Arrays.equals(count1, count2)) {
+            return false;
+        }
         return true;
     }
+
     public static void main(String[] args) {
-        
+        String word1 = "xxxxxxx";
+        String word2 = "zzzzzzz";
+        System.out.println(operation(word1, word2));
     }
 }

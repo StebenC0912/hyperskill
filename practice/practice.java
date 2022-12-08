00f2b07b4b0ed002cbd29c0bd8ca6d107c89b951
StebenC0912 <stebenc09122002@gmail.com>
2022-12-08 22:08:25 +0700

    ko biet j lun

--- practice/practice.java
+++ practice/practice.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 public class practice {
     public static void main(String[] args) {
-        UncheckedException uncheckedException = new UncheckedException();
-        System.out.println(uncheckedException instanceof RuntimeException);
+        
     }
 }
 
-class UncheckedException extends RuntimeException {
+class BadRequestException extends Exception{
 
-    public UncheckedException() {
+    public BadRequestException(String message) {
+        super(message);
     }
-
+    
 }

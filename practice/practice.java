38c4447fdc6f2f34a50f485e90f804e1162ddf6d
StebenC0912 <stebenc09122002@gmail.com>
2022-12-28 23:14:00 +0700

    comment

--- practice/practice.java
+++ practice/practice.java
@@ -39,7 +39,7 @@ class ThreadUtil {
 
     public static void printCurrentThreadInfo() {
         // get the thread and print its info
-        
+        // comment
         System.out.println("name: " + Thread.currentThread().getName());
         System.out.println("priority: " + Thread.currentThread().getPriority());
     }

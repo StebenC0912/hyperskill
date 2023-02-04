afc0db3b096a3c442568e4f63b48ef5b968ae18e
StebenC0912 <stebenc09122002@gmail.com>
2023-02-04 23:00:34 +0700

    commit

--- practice/Main.java
+++ practice/Main.java
@@ -3,21 +3,24 @@ package practice;
 import java.util.Scanner;
 import java.util.Arrays;
 
+
+record Person(String name, int age, String gender) {
+  //don't change this record
+}
+
+
 public class Main {
 
     private static long mainThreadId = Thread.currentThread().getId();
 
     // Fix this method
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        SlowStringProcessor processor = new SlowStringProcessor(str);
-        processor.start();
-        processor.join();
-        System.out.println(processor.getNumberOfUniqueCharacters());
-    }
+        Person person = new Person(scanner.next(), scanner.nextInt(), scanner.next());
 
+        // your code goes here
+        System.out.printf("%s is %d years old and is %s", person.age(), person.age(), person.gender());
+    }
     //Don't change the code below
     static class SlowStringProcessor extends Thread {
 
